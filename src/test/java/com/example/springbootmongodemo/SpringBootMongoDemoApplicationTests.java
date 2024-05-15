f8066470a3ccad0fc0b32ef2f3a2b7fb8d2547e9
Ralph Manlapig <manlapigralph@gmail.com>
2024-05-15 21:38:07 +0800

    Initial Commit

---
+++ src/test/java/com/example/springbootmongodemo/SpringBootMongoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootmongodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMongoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
