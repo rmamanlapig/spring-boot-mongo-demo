e90272e92d839e65ecdff30bfcf3a1cfd5340a10
Ralph Manlapig <manlapigralph@gmail.com>
2024-05-17 19:28:50 +0800

    [feature/improve-APIs] Added homepage redirect to swagger ui

--- src/main/java/com/example/springbootmongodemo/controller/PersonController.java
+++ src/main/java/com/example/springbootmongodemo/controller/PersonController.java
@@ -7,6 +7,8 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,7 +19,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/person")
 public class PersonController {
 
     private final PersonService personService;
@@ -26,16 +27,21 @@ public class PersonController {
         this.personService = personService;
     }
 
+    @RequestMapping(value="/")
+    public void homeRedirect(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/swagger-ui/index.html");
+    }
+
     @Operation(summary = "Add a Person Info", description = "We are adding a Person's info the the DB", tags = {"person", "post"})
     @ApiResponses({@ApiResponse(responseCode = "201", content = {@Content(schema = @Schema(implementation = Person.class), mediaType = "application/json")})})
-    @PostMapping
+    @PostMapping(value="/person")
     public ResponseEntity<Person> addPerson (@RequestBody Person person){
         return new ResponseEntity<>(this.personService.addPerson(person), HttpStatus.CREATED);
     }
 
     @Operation(summary = "Get all Person Info", description = "We are getting all the Person info in the DB", tags = {"person", "get"})
     @ApiResponses({@ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = List.class), mediaType = "application/json")})})
-    @GetMapping
+    @GetMapping(value="/person")
     public ResponseEntity<List<Person>> getAllPerson(){
         return ResponseEntity.ok(this.personService.getAllPerson());
     }
