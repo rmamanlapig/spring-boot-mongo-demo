b87cf1e5419c3f53b13c2c7571a057267b55b706
Ralph Manlapig <manlapigralph@gmail.com>
2024-05-17 18:58:59 +0800

    [v2024-05-15.1.0] Added docker config and resolved "mongoTemplate" issue

--- src/main/java/com/example/springbootmongodemo/Application.java
+++ src/main/java/com/example/springbootmongodemo/Application.java
@@ -1,13 +1,9 @@
 package com.example.springbootmongodemo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class Application {
 
 	public static void main(String[] args) {
