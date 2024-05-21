bb55c56325771a66799f84430ea6ff62ae6f1441
Ralph Manlapig <manlapigralph@gmail.com>
2024-05-21 12:46:37 +0800

    [feature/improve-APIs] Updated APIs

--- src/main/java/com/example/springbootmongodemo/controller/PersonController.java
+++ src/main/java/com/example/springbootmongodemo/controller/PersonController.java
@@ -3,12 +3,15 @@ package com.example.springbootmongodemo.controller;
 import com.example.springbootmongodemo.entities.Person;
 import com.example.springbootmongodemo.service.PersonService;
 import java.util.List;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,14 +24,33 @@ public class PersonController {
         this.personService = personService;
     }
 
-    @PostMapping(value = "/addPerson")
-    @ResponseStatus(HttpStatus.CREATED)
-    public Person addPerson(@RequestBody Person person){
+    @PostMapping(value = "/addPerson", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Person> addPerson(@RequestBody Person person){
         return personService.addPerson(person);
     }
 
-    @GetMapping
-    public List<Person> getPerson(){
+    @GetMapping(value = "/getAllPerson", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Person>> getAllPerson(){
         return personService.getAllPerson();
+    }
+
+    @GetMapping(value = "/getById/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Person> getPersonById(@PathVariable String id){
+        return personService.getPersonById(id);
+    }
+
+    @GetMapping(value = "/getPersonByName/{firstName}/{lastName}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Person>> getPersonByName(@PathVariable String firstName, @PathVariable String lastName){
+        return personService.getPersonByName(firstName, lastName);
+    }
+
+    @PatchMapping(value = "/updatePerson/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Person> updatePerson(@PathVariable String id, @RequestBody Person person){
+        return personService.updatePerson(id, person);
+    }
+
+    @DeleteMapping(value = "/deletePerson/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> deletePerson(@PathVariable String id){
+        return personService.deletePerson(id);
     }
 }
--- src/main/java/com/example/springbootmongodemo/service/PersonService.java
+++ src/main/java/com/example/springbootmongodemo/service/PersonService.java
@@ -2,22 +2,26 @@ package com.example.springbootmongodemo.service;
 
 import com.example.springbootmongodemo.entities.Person;
 import java.util.List;
+import org.springframework.http.ResponseEntity;
 
 public interface PersonService {
 
     // Create Person for DB
-    Person addPerson(Person person);
+    ResponseEntity<Person> addPerson(Person person);
 
-    // Return all Person
-    List<Person> getAllPerson();
-//
-//    // Get Person by ID
-//    Person getPerson(String id);
-//
-//    // Update Person Info
-//    Person updatePersonInfo(String id, Person person);
-//
-//    // Delete Person Info
-//    Person deletePersonInfo(String id);
+    // Return all Person Data in DB
+    ResponseEntity<List<Person>> getAllPerson();
+
+    // Get Person by ID
+    ResponseEntity<Person> getPersonById(String id);
+
+    // Get Person by Full Name
+    ResponseEntity<List<Person>> getPersonByName(String firstName, String lastName);
+
+    // Update Person
+    ResponseEntity<Person> updatePerson(String id, Person person);
+
+    // Delete Person Info
+    ResponseEntity<String> deletePerson(String id);
 
 }
